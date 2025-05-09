6f9abde96e7c2849f772ac8b51d058cd75b3f5ea
Sean Cahill <sean.cahill@students.hs-mainz.de>
2025-05-09 17:39:47 +0200

    added lsps and autosession plugin

--- init.lua
+++ init.lua
@@ -663,7 +663,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -673,8 +673,13 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        ts_ls = {},
+        tailwindcss = {},
+        html = {},
+        cssls = {},
+        jdtls = {},
+        jsonls = {},
+        pyright = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -969,14 +974,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+
+return {
+  'rmagatti/auto-session',
+  lazy = false, -- load on start-up
+  ---@type AutoSession.Config
+  opts = {
+    log_level = 'error',
+    pre_save_cmds = { 'Neotree close' }, -- close tree before snapshot
+    post_restore_cmds = { 'Neotree filesystem reveal' },
+  },
+}
